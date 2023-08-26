86fb0588204acbfc108f993b6225e3376c55a8a4
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2023-08-25 23:37:40 -0300

    🚧 feat: add to-do section

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,27 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="w-full h-full min-h-screen flex flex-col items-center">
+  <div class="w-full h-full sm:px-0 px-2 min-h-screen flex flex-col items-center">
     <div class="flex justify-center w-full mt-[2rem] mb-[3rem]">
-      <h1 class="text-2xl">Pomodoro</h1>
+      <h1 class="text-4xl">Pomodoro</h1>
     </div>
 
-    <div class="grid sm:grid-cols-2 w-full max-w-[80rem] h-full max-h-[40rem]">
-      <div class="flex flex-col bg-neutral p-5 rounded-md">
+    <div class="grid sm:grid-cols-2 gap-4 w-full max-w-[80rem] h-full max-h-[40rem]">
+      <div class="flex flex-col h-[18rem] bg-neutral p-5 rounded-md">
         <div class="flex justify-start">
           <h2 class="text-xl text-white">Timer</h2>
         </div>
         <div class="flex justify-center items-center h-full">
           <Timer />
+        </div>
+      </div>
+
+      <div class="flex flex-col bg-neutral p-5 rounded-md">
+        <div class="flex justify-start">
+          <h2 class="text-xl text-white">To-Do</h2>
+        </div>
+        <div class="flex justify-center items-center h-full">
+          <Todo />
         </div>
       </div>
     </div>
