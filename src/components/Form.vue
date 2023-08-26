9de00a6ba54a1057eaed2373b201e116fef3c010
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2023-08-25 23:39:28 -0300

    ✨ feat: create form component using formkit

---
+++ src/components/Form.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+interface Props {
+  schema?: object;
+  model?: object;
+  options?: any;
+  default?: any;
+  onSubmit?: () => void;
+}
+
+const props = defineProps<Props>();
+
+const emit = defineEmits({
+  submit: () => true
+});
+
+const onSubmit = () => {
+  emit('submit');
+}
+
+</script>
+
+<template>
+  <FormKit
+    type="form"
+    :schema="props.schema"
+    :model="props.model"
+    :options="props.options"
+    :default="props.default"
+    @submit="onSubmit"
+  >
+    <slot />
+  </FormKit>
+</template>
