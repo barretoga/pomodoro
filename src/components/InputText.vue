f0a82a8d72c5aa25da3c8f9b181cf276539c841f
Gabriel Barreto <gabrielbarretogasparelo@gmail.com>
2023-08-28 22:50:32 -0300

    ✨ feat: create input text component

---
+++ src/components/InputText.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+interface Props {
+  label: string;
+  value: string;
+  help?: string;
+  validation?: string;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <FormKit
+    type="text"
+    :label="props.label"
+    :value="props.value"
+    :help="props?.help"
+    :validation="props.validation"
+  />
+</template>
